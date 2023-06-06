5aab5e550808200069cf41fb38c49945d2e00b31
luc perrot <luc.perrot@hotmail.fr>
2023-06-06 17:28:20 +0200

    ajout column dans user et seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "seeding..."
+User.destroy_all
+User.create!(email: "1@example.com", password: "123456", User_name: "1")
+User.create!(email: "2@example.com", password: "123456", User_name: "2")
+puts "done !"
