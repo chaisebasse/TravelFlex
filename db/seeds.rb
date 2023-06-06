f38d17954bef88268adf14e9399ec0eae9368d2c
luc perrot <luc.perrot@hotmail.fr>
2023-06-06 17:11:58 +0200

    Seeds 2 utilisateurs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "seeding..."
+User.create!(email: "1@example.com", password: "123456")
+User.create!(email: "2@example.com", password: "123456")
+puts "seeds done!"
