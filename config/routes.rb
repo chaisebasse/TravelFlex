9a9b5c7c432f098748b943615417ee870c5cb128
valentinecousin <valentine.cousin@gmail.com>
2023-06-06 15:31:33 +0200

    debug routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-<<<<<<< HEAD
   resources :travel
   resources :activity, only: %i[update destroy]
   get "dashboard", to: "pages#dashboard"
-=======
-  resources :travels
-  resources :activities, only: %i[update destroy]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
->>>>>>> master
 end
