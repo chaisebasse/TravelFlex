0a1bb3bec2e343b5ee68b135894a43002b4a4a44
luc perrot <luc.perrot@hotmail.fr>
2023-06-06 17:36:20 +0200

    Bon nom de colomne pour Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 puts "seeding..."
 User.destroy_all
-User.create!(email: "1@example.com", password: "123456", User_name: "1")
-User.create!(email: "2@example.com", password: "123456", User_name: "2")
+User.create!(email: "1@example.com", password: "123456", username: "1")
+User.create!(email: "2@example.com", password: "123456", username: "2")
 puts "done !"
