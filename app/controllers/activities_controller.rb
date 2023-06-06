1762df1eaa2f0f04dca6c4e735bc47e99b728335
Ermanno di Giulio <digiulioermanno@gmail.com>
2023-06-06 14:49:32 +0200

    Controller_activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,20 @@
 class ActivitiesController < ApplicationController
   def update
+    @activity = Activity.find_by(params[:id])
+    @activity.update(restaurant_params)
+    redirect_to activity_path(@activity)
   end
 
   def destroy
+    @activity = Activity.find(params[:id])
+    @activity.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to activity_path, status: :see_other
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:title, :long, :lat, :status, :activity_img_url)
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # ==> before_action :authenticate_user!
+  before_action :authenticate_user!
   # ==> include Pundit::Authorization
 
   # Pundit: allow-list approach
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   # ==> after_action :verify_authorized, except: :index, unless: :skip_pundit?
   # ==> after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-# Uncomment when you *really understand* Pundit!
+  # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   # def user_not_authorized
   #   flash[:alert] = "You are not authorized to perform this action."
