0df76be6965efd15f40238cc29a17a785aad0851
valentinecousin <valentine.cousin@gmail.com>
2023-06-06 15:07:34 +0200

    méthodes controllers application pages travels

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,14 +1,20 @@
 class TravelsController < ApplicationController
+  before_action :set_travel, only: %i[show edit update destroy]
+
   def index
+    @travels = Travel.all
+  end
+
+  def new
+    @travel = Travel.new
   end
 
   def show
   end
 
-  def new
-  end
-
   def create
+    @travel = Travel.new(params_travel)
+    @travel.user = current_user
   end
 
   def edit
@@ -18,5 +24,13 @@ class TravelsController < ApplicationController
   end
 
   def destroy
+    @travel.destroy
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def set_travel
+    @travel = Travel.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
   devise_for :users
   root to: "pages#home"
 
   resources :travel
   resources :activity, only: %i[update destroy]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "dashboard", to: "pages#dashboard"
 end
