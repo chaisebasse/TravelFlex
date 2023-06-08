d7d7076994d9e9e5b2a790fbef6a8a336b57928a
Martin Boulland <martinboulland@gmail.com>
2023-06-08 10:32:37 +0200

    add the image in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,53 +4,70 @@ require "nokogiri"
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home destinations search]
 
-  def home
-    @travels = Travel.all
-
-    classe = ".lazy"
-    country = "liechtenstein"
-    url = "https://www.routard.com/guide/code_dest/#{country}.htm"
-    html_file = URI.open(url).read
-
-    html_doc = Nokogiri::HTML.parse(html_file)
-
-    photo_div = html_doc.css(".home-destination-media-img-wrapper").first
-    target_photo = photo_div.css(classe).first
-    @img_src = target_photo['src']
-
-    outer_div = html_doc.css('.home-dest-desc').first
-    nested_div = outer_div.css('div[style]').first
-    p_tag = nested_div.css('p')[0]
-    text_before_br = p_tag.children.select { |node| node.name == 'text' }.first
-    @text_content = text_before_br.text.strip
-  end
 
 
-def search_results
-  client = OpenAI::Client.new
-  response = client.completions(
-    parameters: {
-      model: "text-davinci-003",
-      prompt: 'Give me an array of 5 destinations, budget #{params[:budget]} for a
-      #{params[:type_of_travelers]} #{params[:type_of_destination]} #{params[:duration]} max 10 token / [{"country":, "city":, "lat":, "long":}]',
-      max_tokens: 350
-  })
-  choices = response['choices']
-  destinations = response['choices'][0]['text']
-  @destinations_array = JSON.parse(destinations)
-  redirect_to destinations_path(result:@destinations_array)
+  def search_results
+    client = OpenAI::Client.new
+    response = client.completions(
+      parameters: {
+        model: "text-davinci-003",
+        prompt: 'Donne moi un array de 5 destinations, budget #{params[:budget]} pour un
+        #{params[:type_of_travelers]},  #{params[:type_of_destination]}, pour une durée de #{params[:duration]} max 10 token / [{"pays":, "ville":, "lat":, "long":}]',
+        max_tokens: 400
+        })
+        choices = response['choices']
+        destinations = response['choices'][0]['text']
+        destinations_array = JSON.parse(destinations)
+        final_array = scraping(destinations_array)
+        # raise
+       redirect_to destinations_path(result:final_array)
 end
 
- def dashboard
-    if user_signed_in?
-      @travels = current_user.travels
+def destinations
+end
+
+
+def scraping(destinations)
+  response = []
+    destinations.each do | destination|
+    if destination["pays".downcase] == "France" || destination["pays".parameterize] == "royaume-uni"
     else
-      redirect_to new_user_session_path
+      classe = ".lazy"
+      country = "#{destination["pays"].parameterize}"
+      country.strip!
+      country = country.gsub!(/[-]/, '_') if country.include?("-")
+      url = "https://www.routard.com/guide/code_dest/#{country}.htm"
+      html_file = URI.open(url).read
+      html_doc = Nokogiri::HTML.parse(html_file)
+      photo_div = html_doc.css(".home-destination-media-img-wrapper").first
+      target_photo = photo_div.css(classe).first
+      img_src = target_photo['src']
+      outer_div = html_doc.css('.home-dest-desc').first
+      nested_div = outer_div.css('div[style]').first
+      p_tag = nested_div.css('p')[0]
+      # text_before_br = p_tag.children.select { |node| node.name == 'text' }.first
+      # @text_content = text_before_br.text.strip
+
+      destination["img_src"] = img_src
+      # destination["@text_content"] = img_src
     end
+    response << destination
+
+  # soluce quand pas de pays trouvé sur html_file
+end
+
+  return response
+raise
+end
+
+
+def dashboard
+  if user_signed_in?
+    @travels = current_user.travels
+  else
+    redirect_to new_user_session_path
   end
 
-  def destinations
-    end
 
 
   # def search
@@ -69,4 +86,5 @@ end
   # def travel_params
   #   params.require(:travel).permit(:theme, :duration, :budget, :travelers, :starting_date)
   # end
+end
 end
