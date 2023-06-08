53ff0621c943c37787a9c23a21a704522a4c44ab
valentinecousin <valentine.cousin@gmail.com>
2023-06-08 15:11:16 +0200

    jai stické la navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,6 @@ require "nokogiri"
 
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home destinations search]
-
   # classe = ".lazy"
   # country = "paris"
   # url = "https://www.routard.com/guide/code_dest/#{country}.htm"
@@ -49,9 +48,9 @@ class PagesController < ApplicationController
         target_photo = photo_div.css(classe).first
         img_src = target_photo['src']
 
-        # outer_div = html_doc.css('.home-dest-desc').first
-        # nested_div = outer_div.css('div[style]').first
-        # p_tag = nested_div.css('p')[0]
+        outer_div = html_doc.css('.home-dest-desc').first
+        nested_div = outer_div.css('div[style]').first
+        p_tag = nested_div.css('p')[0]
         # text_before_br = p_tag.children.select { |node| node.name == 'text' }.first
         # @text_content = text_before_br.text.strip
 
