147be5623214a1a154cb8c17ec1f353c8d9c353f
valentinecousin <valentine.cousin@gmail.com>
2023-06-09 15:41:42 +0200

    form du carousel modifié 1st step travail en cours

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,8 @@
+// import { Controller } from "@hotwired/stimulus"
+// import flatpickr from "flatpickr";
+
+// // Connects to data-controller="flatpickr"
+// export default class extends Controller {
+//   connect() {
+//   }
+// }
