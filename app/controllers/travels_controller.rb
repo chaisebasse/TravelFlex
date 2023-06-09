321800fcfc2146af7df5d5b2a7e276cfbe3a58c7
Ermanno di Giulio <digiulioermanno@gmail.com>
2023-06-09 09:19:00 +0200

    jsp

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -2,7 +2,6 @@ class TravelsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[new]
   before_action :set_travel, only: %i[show edit update destroy]
 
-
   def index
     @travels = Travel.all
   end
