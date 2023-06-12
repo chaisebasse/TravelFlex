3b2ed8faf9132340bacc89982688fac14ea6aad6
Ermanno di Giulio <digiulioermanno@gmail.com>
2023-06-12 11:05:09 +0200

    scraper ajouté dans scraper_test.rb

--- scraper_test.rb
+++ scraper_test.rb
@@ -2,10 +2,12 @@ def scraping(destinations)
   response = []
   destinations.each do |destination|
     classe = ".lazy"
-    region = destination["pays"].parameterize
+    region = destination["region"].parameterize
     region.strip!
-
     region = region.gsub!(/-/, '_') if region.include?("-")
+    country = destination["country"].parameterize
+    country.strip!
+    country = country.gsub!(/-/, '_') if country.include?("-")
 
     url = "https://www.routard.com/guide/code_dest/#{region}.htm"
     html_file = URI.open(url).read
@@ -34,11 +36,22 @@ def scraping(destinations)
 
       p_tag_select = html_doc.search('.community p')
       if p_tag_select.css('.lieu-intro')
-        p_tag= p_tag_select.html_doc.css('.lieu-intro').first
+        p_tag = p_tag_select.html_doc.css('.lieu-intro').first
       else
         p_tag = p_tag_select.reject { |par| !par.css('.lieu-intro').nil? }.first
       end
     else
+      url = "https://www.routard.com/guide/code_dest/#{country}.htm"
+      html_file = URI.open(url).read
+      html_doc = Nokogiri::HTML.parse(html_file)
+
+      main_div = html_doc.css(".home-destination-media-img-wrapper").first
+      target_photo = main_div.css(classe).first
+      img_src = target_photo['src']
+
+      div = html_doc.search('.home-dest-desc p')
+      filtered_paragraphes = div.reject { |par| !par.at('strong').nil? }
+      p_tag = filtered_paragraphes.first
     end
 
     text_before_br = p_tag.children.select { |node| node.name == 'text' }.first
