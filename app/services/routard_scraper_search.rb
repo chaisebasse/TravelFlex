3f6af2382d9fdcb38f3895f728a7977c8c00ba96
Ermanno di Giulio <digiulioermanno@gmail.com>
2023-06-16 09:48:31 +0200

    4 destinations

--- app/services/routard_scraper_search.rb
+++ app/services/routard_scraper_search.rb
@@ -1,37 +1,37 @@
-require "open-uri"
-require "nokogiri"
-require 'google_search_results'
+# require "open-uri"
+# require "nokogiri"
+# require 'google_search_results'
 
-class RoutardScraperSearch
-  def initialize(region, country)
-    @region = region
-    @country = country
-    GoogleSearch.api_key = ENV['GOOGLESEARCH_API_KEY']
-  end
+# class RoutardScraperSearch
+#   def initialize(region, country)
+#     @region = region
+#     @country = country
+#     GoogleSearch.api_key = ENV['GOOGLESEARCH_API_KEY']
+#   end
 
-  def call
-    begin
-      client = GoogleSearch.new(q: "site:routard.com #{@region}")
-      search_results = client.get_hash
-      title_regex = /\AVisiter \D+(\D+)*, Voyage \D+/
-      result = search_results[:organic_results].find { |r| r[:title].match(title_regex) }
-      result_url = result[:link] if result
-      html_doc = Nokogiri::HTML(URI.open(result_url))
+#   def call
+#     begin
+#       client = GoogleSearch.new(q: "site:routard.com #{@region}")
+#       search_results = client.get_hash
+#       title_regex = /\AVisiter \D+(\D+)*, Voyage \D+/
+#       result = search_results[:organic_results].find { |r| r[:title].match(title_regex) }
+#       result_url = result[:link] if result
+#       html_doc = Nokogiri::HTML(URI.open(result_url))
 
-      main_div = html_doc.css(".community").first
-      target_photo = main_div.css(".lazy").first
-      img_src = target_photo['src']
+#       main_div = html_doc.css(".community").first
+#       target_photo = main_div.css(".lazy").first
+#       img_src = target_photo['src']
 
-      p_tag_select = main_div.search('p')
+#       p_tag_select = main_div.search('p')
 
-      if p_tag_select.at_css('.lieu-intro').text.present?
-        p_tag = p_tag_select.at_css('.lieu-intro').text.strip
-      else
-        p_tag = p_tag_select.map(&:text).find(&:present?).text.strip
-      end
-      return [img_src, p_tag.scan(/[^.]*?(?:\.+|\u2026)(?![.])/)]
-    rescue => e
-      RoutardScraperExtra.new(@region, @country).call
-    end
-  end
-end
+#       if p_tag_select.at_css('.lieu-intro').text.present?
+#         p_tag = p_tag_select.at_css('.lieu-intro').text.strip
+#       else
+#         p_tag = p_tag_select.map(&:text).find(&:present?).text.strip
+#       end
+#       return [img_src, p_tag.scan(/[^.]*?(?:\.+|\u2026)(?![.])/)]
+#     rescue => e
+#       RoutardScraperExtra.new(@region, @country).call
+#     end
+#   end
+# end
